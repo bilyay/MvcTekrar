29ce052982b0fc62610bf239aabbd16d147e239a
Abdurrahman yay <bil-yay@hotmail.com>
2016-11-28 16:09:12 +0200

    Add project files.

---
+++ WebApplication6/Models/ORM/Entity/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models.ORM.Entity
+{
+    public class BaseEntity
+    {
+        public int ID { get; set; }
+        private DateTime _addDate { get; set; }
+        public DateTime AddDate
+        {
+            get { return _addDate; }
+            set { _addDate = DateTime.Now; }
+
+        }
+        private bool _isDeleted { get; set; }
+        public bool IsDeleted
+        {
+            get { return _isDeleted; }
+            set { _isDeleted = false; }
+        }
+        public DateTime? DeleteDate { get; set; }
+    }
+}
